9b2c8dd0338b2092b302d01d53a7a807515df508
Emmanuel <ovizcayag@gmail.com>
2026-08-27 12:37:00 -0400

    roles y permisos 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,15 +19,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="es" suppressHydrationWarning>
+      <body className="antialiased">
+        {children}
+      </body>
     </html>
   );
 }
